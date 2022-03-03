dfbc5464460d23fa7de8b8c519b044b0fa16d548
Gjorgjioska <marina.gorgioska@gmail.com>
2022-03-03 18:18:38 +0100

    Updating auth state

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "7657517299-pklr150q2uhahst1gjsohlekpp0f7ink.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.onAuthChange();
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
+    });
+  }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I dont know of we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div> I am signed in!</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+export default GoogleAuth;
